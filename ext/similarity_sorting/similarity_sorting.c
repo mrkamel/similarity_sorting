781a96b173dbeb646b534103d7e6e1202d6dee9b
Benjamin Vetter <vetter@plainpicture.de>
2017-02-24 15:37:19 +0100

    Merging half sized blocks

--- ext/similarity_sorting/similarity_sorting.c
+++ ext/similarity_sorting/similarity_sorting.c
@@ -13,47 +13,51 @@ void Init_similarity_sorting() {
   rb_define_singleton_method(SimilaritySorting, "sort", method_similarity_sorting_sort, 3);
 }
 
+void sort_block(VALUE array, VALUES scores, long start, long stop) {
+  for(i = start + 1; i < stop; i++) {
+    VALUE reference = rb_ary_entry(array, i - 1);
+    VALUE reference_keywords_array = rb_ary_entry(reference, 2);
+
+    long max_index = i;
+    double max_value = -1;
+    long u;
+
+    for(u = i; u < m; u++) {
+      VALUE current = rb_ary_entry(array, u);
+      VALUE current_keywords_hash = rb_ary_entry(current, 1);
+      long keywords_count = RARRAY_LEN(reference_keywords_array);
+      double value = 0;
+      long k;
+
+      for(k = 0; k < keywords_count; k++) {
+        VALUE keyword = rb_ary_entry(reference_keywords_array, k);
+
+        if(rb_hash_lookup(current_keywords_hash, keyword) == Qtrue)
+          value += NUM2DBL(rb_ary_entry(scores, NUM2LONG(keyword)));
+      }
+
+      if(value > max_value) {
+        max_index = u;
+        max_value = value;
+      }
+    }
+
+    VALUE old = rb_ary_entry(array, i);
+    rb_ary_store(array, i, rb_ary_entry(array, max_index));
+    rb_ary_store(array, max_index, old);
+  }
+}
+
 VALUE method_similarity_sorting_sort(VALUE self, VALUE array, VALUE scores, VALUE lookahead) {
   long len = RARRAY_LEN(array);
   long lookahead_value = NUM2LONG(lookahead);
   long b;
 
-  for(b = 0; b < len; b += lookahead_value) {
-    long i, m = MIN(len, b + lookahead_value);
+  for(b = 0; b < len; b += lookahead_value)
+    sort_block(array, scores, b, MIN(len, b + lookahead_value);
 
-    for(i = b + 1; i < m; i++) {
-      VALUE reference = rb_ary_entry(array, i - 1);
-      VALUE reference_keywords_array = rb_ary_entry(reference, 2);
-
-      long max_index = i;
-      double max_value = -1;
-      long u;
-
-      for(u = i; u < m; u++) {
-        VALUE current = rb_ary_entry(array, u);
-        VALUE current_keywords_hash = rb_ary_entry(current, 1);
-        long keywords_count = RARRAY_LEN(reference_keywords_array);
-        double value = 0;
-        long k;
-
-        for(k = 0; k < keywords_count; k++) {
-          VALUE keyword = rb_ary_entry(reference_keywords_array, k);
-
-          if(rb_hash_lookup(current_keywords_hash, keyword) == Qtrue)
-            value += NUM2DBL(rb_ary_entry(scores, NUM2LONG(keyword)));
-        }
-
-        if(value > max_value) {
-          max_index = u;
-          max_value = value;
-        }
-      }
-
-      VALUE old = rb_ary_entry(array, i);
-      rb_ary_store(array, i, rb_ary_entry(array, max_index));
-      rb_ary_store(array, max_index, old);
-    }
-  }
+  for(b = lookahead_value / 2; b < len; b += lookahead_value)
+    sort_block(array, scores, b, MIN(len, b + lookahead_value);
 
   return array;
 }
